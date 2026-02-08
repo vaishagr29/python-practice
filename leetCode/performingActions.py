e5942a02d602a86b29caa6d44f65ed21ae97fb12
Vaishnavi Agrawal <vaishnaviagrawal2912@gmail.com>
2026-02-08 13:14:05 +0530

    done.

---
+++ leetCode/performingActions.py
@@ -0,0 +1,21 @@
+# Given an integer, n , perform the following conditional actions:
+
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if(n%2==0):
+    if(n>=2 and n<=5):
+        print("Not Weird")
+    elif(n>5 and n<21):
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird")
+
+
